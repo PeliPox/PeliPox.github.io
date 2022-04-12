62df37f9e7b128694f5dd16ae8c8e10b341b85a1
PeliPox <97631290+PeliPox@users.noreply.github.com>
2022-04-12 23:23:49 +0300

    Add files via upload

---
+++ web/src/js/testScript.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let arr = [
+    "One",
+    "Two",
+    15,
+    true,
+    function () {
+        console.log("its work");
+    }
+];
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+if(confirm("BADASS!")){
+    console.log("NOT YOU :)");
+}
